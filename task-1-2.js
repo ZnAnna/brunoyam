2b34f5260672fc5499237cbcfdf6c6930b534317
ZnAnna <zn_anya@mail.ru>
2025-02-17 18:53:53 +0300

    second commit hm

--- task-1-2.js
+++ task-1-2.js
@@ -1,4 +1,4 @@
 let persons = ["Иван", "Петя", "Олег"];
-persons[0] = persons[0] + " Петров";
-persons[1] = persons[1] + " Сидоров";
-persons[2] = persons[2] + " Иванов";
+persons[0] += " Петров";
+persons[1] += " Сидоров";
+persons[2] += " Иванов";
--- task-2-1.js
+++ task-2-1.js
@@ -1,12 +1,11 @@
 let films = ["3 Билборда на границе Эббинга, Миссури", "Король говорит", "Гран Торино", "Город Проклятых", "Титаник", "Хатико"];
 let userAnswer = [];
 for (let i = 0; i < films.length; i++) {
-     question = confirm(`Смотрели ли вы "${films[i]}"?`);
+     const question = confirm(`Смотрели ли вы "${films[i]}"?`);
         if (question == true) {
-            userAnswer.push(`${films[i]}: Да\n`);                   
+            userAnswer.push(`${i + 1}. ${films[i]}: Да\n`);                   
         } else {
-            userAnswer.push(`${films[i]}: Нет\n`);  
+            userAnswer.push(`${i + 1}. ${films[i]}: Нет\n`);  
         }
 }
-alert(userAnswer);
-// не могу убрать запятую перед наименованием фильма и пронумеровать список
+alert(userAnswer.join(''));
